332d79ee481f27e0167603714d75f687d3898239
Akarsh-Kumar-Jha <akarshdbg@gmail.com>
2025-12-31 22:35:26 +0530

    Updated Caching

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,20 @@ import FeaturedBlogs from '@/components/FeaturedBlogs'
 import FeaturedProjects from '@/components/FeaturedProjects'
 import LandingPage from '@/components/LandingPage'
 import RandomQuote from '@/components/RandomQuote'
-import React from 'react'
+import React, { Suspense } from 'react'
 
-function HomePage() {
+async function HomePage() {
   return (
-   <div className='w-full flex flex-col gap-y-16 md:gap-y-8'>
+   <div className='w-full flex flex-col gap-y-16 md:gap-y-8 lg:gap-y-5'>
      <LandingPage />
      <FeaturedProjects />
      <AboutMe />
-     <FeaturedBlogs />
-     <RandomQuote />
+    <Suspense fallback={<div>Loading...</div>}>
+       <FeaturedBlogs />
+    </Suspense>
+     <Suspense fallback={<div>Loading...</div>}>
+      <RandomQuote />
+     </Suspense>
    </div>
   )
 }
