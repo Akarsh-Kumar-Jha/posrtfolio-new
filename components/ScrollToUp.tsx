b65337117920488176638047288220fe6b760a73
Akarsh-Kumar-Jha <akarshdbg@gmail.com>
2026-01-12 17:25:58 +0530

    Changed Scroll up button style

--- components/ScrollToUp.tsx
+++ components/ScrollToUp.tsx
@@ -4,7 +4,7 @@ import { BiUpArrowCircle } from 'react-icons/bi';
 
 function ScrollToUp() {
   return (
-    <BiUpArrowCircle onClick={() => window.scrollTo({top:10,behavior:'smooth'})} className='text-4xl text-accent animate-ping cursor-pointer' />
+    <BiUpArrowCircle onClick={() => window.scrollTo({top:10,behavior:'smooth'})} className='text-4xl text-accent animate cursor-pointer' />
   )
 }
 
